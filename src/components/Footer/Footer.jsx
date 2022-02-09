4233dbb1d07f0e09a936176051635a07e3ee3eb1
Alifanov Valentin <va.alifanov@it-justice.com>
2022-02-09 17:52:39 +0300

    versus_3

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,8 @@ export default function Footer () {
                 </div>
             </div>
             <div className='footer-down-box'>
-
+                <p className='footer-down__text1'>© 2021 Justice-it. All rights reserved.</p>
+                <p className='footer-down__text2'>© 2021 Justice-it. All rights reserved.</p>
             </div>
         </footer>
 )
